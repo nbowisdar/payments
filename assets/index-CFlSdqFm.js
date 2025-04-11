77411bb162e8166d87ebff2c51ef6d730af4cadf
Volodymyr F <vova@asap.com>
2025-04-11 18:58:14 +0300

    Updates

--- assets/index-CFlSdqFm.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-t8o8zbhO.js";const e=function(){return n.jsx("div",{children:n.jsx("a",{href:"/payments"})})};export{e as component};
---
+++ assets/index-DxkXofg0.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CvI9KlrU.js";const e=function(){return n.jsx("div",{children:n.jsx("a",{href:"/payments"})})};export{e as component};
