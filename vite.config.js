e44cc6884cf62f3b0e94f628852693533f403d71
Volodymyr F <vova@asap.com>
2025-04-11 18:27:52 +0300

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
