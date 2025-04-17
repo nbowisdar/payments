7a9cc2ee7803136ccd8021fa079430b938d6035a
Volodymyr F <vova@asap.com>
2025-04-17 17:01:37 +0300

    Updates

---
+++ assets/gateway-api-DEq07xT5.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-t_567M37.js";const e=function(){return o.jsx("div",{children:"This is Gateway page!"})};export{e as component};
---
+++ assets/index-CfLXa38r.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-t_567M37.js";const e=function(){return n.jsx("div",{children:n.jsx("a",{href:"/payments"})})};export{e as component};
